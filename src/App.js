d2532398f3b61052f5ca05f3982d28d5eb37bf47
jaychodvadiy <jaychodvadiya130@gmail.com>
2024-12-29 00:19:40 +0530

    chang  and remove tailwind  packge install   add file and crate file

--- src/App.js
+++ src/App.js
@@ -1,41 +1,111 @@
-import React, { useState } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Footer from "./components/footer";
-import HomePage from "./pages/HomePage";
-import ProductPage from "./pages/ProductPage";
-import CartPage from "./pages/CartPage";
-import CheckoutPage from "./pages/CheckoutPage";
-import PaymentSuccessPage from "./pages/PaymentSuccessPage";
-import RegisterPage from "./pages/RegisterPage";
-import LoginPage from "./pages/LoginPage";
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import OurStore from "./pages/OurStore";
+import Blog from "./pages/Blog";
+import CompareProduct from "./pages/CompareProduct";
+import Wishlist from "./pages/Wishlist";
+import Login from "./pages/Login";
+import Forgotpassword from "./pages/Forgotpassword";
+import Signup from "./pages/Signup";
+import Resetpassword from "./pages/Resetpassword";
+import SingleBlog from "./pages/SingleBlog";
+import PrivacyPolicy from "./pages/PrivacyPolicy";
+import RefundPloicy from "./pages/RefundPloicy";
+import ShippingPolicy from "./pages/ShippingPolicy";
+import TermAndContions from "./pages/TermAndContions";
+import SingleProduct from "./pages/SingleProduct";
+import Cart from "./pages/Cart";
+import Checkout from "./pages/Checkout";
+import { PrivateRoutes } from "./routing/PrivateRoutes";
+import { OpenRoutes } from "./routing/OpenRoutes";
+import Orders from "./pages/Orders";
+import Profile from "./pages/Profile";
 
 function App() {
-  const [cart, setCart] = useState([]);
-
-  const addToCart = (product) => setCart([...cart, product]);
-  const removeFromCart = (id) => setCart(cart.filter((item) => item.id !== id));
-
   return (
-    <Router>
-      <Navbar cartCount={cart.length} />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route
-          path="/products"
-          element={<ProductPage addToCart={addToCart} />}
-        />
-        <Route
-          path="/cart"
-          element={<CartPage cart={cart} removeFromCart={removeFromCart} />}
-        />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/checkout" element={<CheckoutPage cart={cart} />} />
-        <Route path="/success" element={<PaymentSuccessPage />} />
-      </Routes>
-      <Footer />
-    </Router>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="about" element={<About />} />
+            <Route path="contact" element={<Contact />} />
+            <Route path="product" element={<OurStore />} />
+            <Route path="product/:id" element={<SingleProduct />} />
+            <Route path="blogs" element={<Blog />} />
+            <Route path="blog/:id" element={<SingleBlog />} />
+            <Route
+              path="cart"
+              element={
+                <PrivateRoutes>
+                  <Cart />
+                </PrivateRoutes>
+              }
+            />
+            <Route
+              path="my-orders"
+              element={
+                <PrivateRoutes>
+                  <Orders />
+                </PrivateRoutes>
+              }
+            />
+            <Route
+              path="my-profile"
+              element={
+                <PrivateRoutes>
+                  <Profile />
+                </PrivateRoutes>
+              }
+            />
+            <Route
+              path="checkout"
+              element={
+                <PrivateRoutes>
+                  <Checkout />
+                </PrivateRoutes>
+              }
+            />
+            <Route path="compare-product" element={<CompareProduct />} />
+            <Route
+              path="wishlist"
+              element={
+                <PrivateRoutes>
+                  <Wishlist />
+                </PrivateRoutes>
+              }
+            />
+            <Route
+              path="login"
+              element={
+                <OpenRoutes>
+                  <Login />
+                </OpenRoutes>
+              }
+            />
+            <Route path="forgot-password" element={<Forgotpassword />} />
+            <Route
+              path="signup"
+              element={
+                <OpenRoutes>
+                  <Signup />
+                </OpenRoutes>
+              }
+            />
+            <Route path="reset-password/:token" element={<Resetpassword />} />
+            <Route path="privacy-policy" element={<PrivacyPolicy />} />
+            <Route path="refund-policy" element={<RefundPloicy />} />
+            <Route path="shipping-policy" element={<ShippingPolicy />} />
+            <Route path="term-conditions" element={<TermAndContions />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,51 +1,136 @@
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import React, { useEffect } from "react";
+import ReactStars from "react-rating-stars-component";
+import { useLocation, useNavigate } from "react-router-dom";
 
-function ProductCard({ product, addToCart }) {
-  const [showMessage, setShowMessage] = useState(false);
+import wish from "../images/wish.svg";
+// import wishlist from "../images/wishlist.svg";
+// import watch from "../images/watch.jpg";
+// import watch2 from "../images/watch-1.avif";
+// import addcart from "../images/add-cart.svg";
+import view from "../images/view.svg";
+import { useDispatch, useSelector } from "react-redux";
+import { addToWishlist } from "../features/products/productSlilce";
+import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
+import { useState } from "react";
+
+const ProductCard = (props) => {
   const navigate = useNavigate();
+  const { grid, data } = props;
+  const dispatch = useDispatch();
+  console.log(data);
+  const location = useLocation();
 
-  const handleBuyNow = () => {
-    addToCart(product);
-    navigate("/checkout");
+  const wishlistState = useSelector((state) => state?.auth?.wishlist?.wishlist);
+
+  const [wishlist, setWishlist] = useState(wishlistState || []);
+
+  useEffect(() => {
+    setWishlist(wishlistState || []);
+  }, [wishlistState]);
+
+  const isProductInWishlist = (productId) => {
+    return wishlist?.some((item) => item._id === productId);
   };
 
-  const handleAddToCart = () => {
-    addToCart(product);
-    setShowMessage(true);
-    setTimeout(() => setShowMessage(false), 2000); 
+  const addToWish = (productId) => {
+    if (isProductInWishlist(productId)) {
+      dispatch(addToWishlist(productId)); // Dispatch the action to update the wishlist in Redux store
+
+      const updatedWishlist = wishlist.filter((item) => item._id !== productId);
+      setWishlist(updatedWishlist);
+    } else {
+      dispatch(addToWishlist(productId)); // Dispatch the action to update the wishlist in Redux store
+
+      const product = data.find((item) => item._id === productId);
+      setWishlist([...wishlist, product]);
+    }
   };
 
   return (
-    <div className="bg-white shadow-md rounded-lg p-4 hover:shadow-lg transition">
-      <img
-        src={product.image}
-        alt={product.name}
-        className="w-full h-40 object-cover rounded-t-lg"
-      />
-      <div className="mt-4">
-        <h2 className="text-xl font-semibold text-gray-800">{product.name}</h2>
-        <p className="text-gray-600 mt-2">${product.price}</p>
-        <div className="flex space-x-2 mt-4">
-          <button
-            onClick={handleAddToCart}
-            className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
+    <>
+      {data?.map((item, index) => {
+        const isWishlist = isProductInWishlist(item._id);
+        console.log(isWishlist);
+        return (
+          <div
+            key={index}
+            className={` ${
+              location.pathname == "/product" ? `gr-${grid}` : "col-3"
+            } `}
           >
-            Add to Cart
-          </button>
-          <button
-            onClick={handleBuyNow}
-            className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600"
-          >
-            Buy Now
-          </button>
-        </div>
-        {showMessage && (
-          <div className="mt-2 text-green-500 text-sm">Item added to cart!</div>
-        )}
-      </div>
-    </div>
+            <div className="product-card position-relative">
+              <div className="wishlist-icon position-absolute">
+                <button
+                  className="border-0 bg-transparent"
+                  onClick={(e) => addToWish(item?._id)}
+                >
+                  {isWishlist ? (
+                    <AiFillHeart className="fs-5 me-1" />
+                  ) : (
+                    <AiOutlineHeart className="fs-5 me-1" />
+                  )}
+                </button>
+              </div>
+
+              <div className="product-image">
+                <img
+                  src={item?.images[0]?.url}
+                  // className="img-fluid d"
+                  alt="product image"
+                  height={"250px"}
+                  width={"100%"}
+                  onClick={() => navigate("/product/" + item?._id)}
+                />
+                <img
+                  src={item?.images[0]?.url}
+                  // className="img-fluid d"
+                  alt="product image"
+                  height={"250px"}
+                  width={"100%"}
+                  onClick={() => navigate("/product/" + item?._id)}
+                />
+              </div>
+              <div className="product-details">
+                <h6 className="brand">{item?.brand}</h6>
+                <h5 className="product-title">
+                  {grid === 12 || grid === 6
+                    ? item?.title
+                    : item?.title?.substr(0, 80) + "..."}
+                </h5>
+                <ReactStars
+                  count={5}
+                  size={24}
+                  value={item?.totalrating}
+                  edit={false}
+                  activeColor="#ffd700"
+                />
+
+                <p className="price">Rs.{item?.price}</p>
+              </div>
+              <div className="action-bar position-absolute">
+                <div className="d-flex flex-column gap-15">
+                  {/* <button className="border-0 bg-transparent">
+                    <img src={prodcompare} alt="compare" />
+                  </button> */}
+
+                  {/* <button className="border-0 bg-transparent">
+                    <img
+                      onClick={() => navigate("/product/" + item?._id)}
+                      src={view}
+                      alt="view"
+                    />
+                  </button> */}
+                  {/* <button className="border-0 bg-transparent">
+                    <img src={addcart} alt="addcart" />
+                  </button> */}
+                </div>
+              </div>
+            </div>
+          </div>
+        );
+      })}
+    </>
   );
-}
+};
 
 export default ProductCard;
